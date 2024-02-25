b875c831e7b2a66624d510d0b7e6da5f5444ecfb
Rodrigo Romero <rodrigoromero2308@gmail.com>
2024-02-25 17:39:51 -0300

    added android assets

---
+++ chirplyfy/android/app/src/main/java/ar/com/rodrigoromero/chirplyfy/MainActivity.java
@@ -0,0 +1,5 @@
+package ar.com.rodrigoromero.chirplyfy;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
--- chirplyfy/capacitor.config.ts
+++ chirplyfy/capacitor.config.ts
@@ -1,12 +1,12 @@
-import { CapacitorConfig } from '@capacitor/cli';
+import { CapacitorConfig } from "@capacitor/cli";
 
 const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
-  appName: 'Chirplyfy',
-  webDir: 'dist',
+  appId: "ar.com.rodrigoromero.chirplyfy",
+  appName: "Chirplyfy",
+  webDir: "dist",
   server: {
-    androidScheme: 'https'
-  }
+    androidScheme: "https",
+  },
 };
 
 export default config;
