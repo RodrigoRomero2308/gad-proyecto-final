b2b879cc58b6ccb3fb5eec4e0f539c49d68d5157
Rodrigo Romero <rodrigoromero2308@gmail.com>
2024-02-29 17:39:49 -0300

    added placeholder when search returns no result

--- chirplyfy/src/components/BirdSongsList.tsx
+++ chirplyfy/src/components/BirdSongsList.tsx
@@ -16,15 +16,38 @@ const BirdSongsList = ({
         <IonIcon slot="start" icon={returnUpBackOutline} />
         <IonLabel>Reset</IonLabel>
       </IonButton>
-      <IonList
-        style={{
-          background: "transparent",
-        }}
-      >
-        {birdSongs.map((item) => (
-          <BirdSongItem key={item.id} birdSong={item} />
-        ))}
-      </IonList>
+      {birdSongs.length ? (
+        <IonList
+          style={{
+            background: "transparent",
+          }}
+        >
+          {birdSongs.map((item) => (
+            <BirdSongItem key={item.id} birdSong={item} />
+          ))}
+        </IonList>
+      ) : (
+        <div>
+          <IonLabel
+            color="primary"
+            className="ion-margin"
+            style={{
+              display: "block",
+            }}
+          >
+            <h2>No feathered friends found nearby.</h2>
+          </IonLabel>
+          <IonLabel
+            color="secondary"
+            className="ion-margin"
+            style={{
+              display: "block",
+            }}
+          >
+            <h3>Expand your search radius or come back later.</h3>
+          </IonLabel>
+        </div>
+      )}
     </>
   );
 };
